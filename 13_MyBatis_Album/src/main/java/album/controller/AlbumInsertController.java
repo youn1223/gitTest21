4ee4060033a7b325297265d23c7a40255825670f
mymellania <mymellania1223@gmail.com>
2025-03-19 17:39:13 +0900

    kim 5:38 수정

--- 13_MyBatis_Album/src/main/java/album/controller/AlbumInsertController.java
+++ 13_MyBatis_Album/src/main/java/album/controller/AlbumInsertController.java
@@ -26,7 +26,7 @@ public class AlbumInsertController {
 	// list.jsp에서 추가하기 버튼
 	@RequestMapping(value=command, method = RequestMethod.GET)
 	public String doActionGet() {
-		return getPage;
+		return "abcd"; // kim이 수정함
 		// /WEB-INF/album/albumInsertForm.jsp
 	}
 	
